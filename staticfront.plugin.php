21ca40767f47717aa2b03326cc8c49fd9477146c
rickc <USER@DOMAIN>
2008-08-14 15:21:23 +0000

    Fix for the rewrite rules so posts display properly at the adjusted url. Also added an option to display static pages at either the base url or the adjusted url.
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/staticfront/trunk@745 f4a90942-1786-4aa1-b092-57c31935ddc3

--- staticfront.plugin.php
+++ staticfront.plugin.php
@@ -3,7 +3,7 @@
 class StaticFront extends Plugin
 {
 
-	const VERSION= '0.1';
+	const VERSION= '0.2';
 
 	/**
 	 * Return plugin metadata for this plugin
@@ -24,12 +24,12 @@ class StaticFront extends Plugin
 	
 	public function action_plugin_activation( $file )
 	{
-		if ( $file == $this->get_file() ) {
+		if ( realpath( $file ) == __FILE__ ) {
 			Options::set( 'staticfront__page', 'none' );
 			Options::set( 'staticfront__blog_index', 'blog' );
+			Options::set( 'staticfront__keep_pages', TRUE );
 		}
 	}
-
 
 	/**
 	 * Add update beacon support
@@ -38,7 +38,7 @@ class StaticFront extends Plugin
 	{
 	 	Update::add( 'StaticFront', 'cc3fd1b0-3bca-11dd-ae16-0800200c9a66', $this->info->version );
 	}
-	
+
 	/* Set up options */
 	public function filter_plugin_config( $actions, $plugin_id )
 	{
@@ -62,40 +62,86 @@ class StaticFront extends Plugin
 					$blog_index= $ui->append( 'text', 'blog_index', 'staticfront__blog_index', sprintf( _t('Show normal posts at this URL: <b>%s</b>', 'staticfront'), Site::get_url( 'habari', true ) ) );
 
 					$blog_index->add_validator( 'validate_required' );
+
+					$keep_pages= $ui->append( 'checkbox', 'keep_pages', 'staticfront__keep_pages', _t( 'Show static pages at base url: ', 'staticfront' ) );
+
 					$ui->append( 'submit', 'save', _t('Save') );
-					$ui->out();
+					$ui->out(); 
 					break;
 			}
 		}
 	}
-	
+
 	private function get_all_pages()
 	{
 		$pages= Posts::get( array( 'content_type' => Post::type('page'), 'nolimit' => 1 ) );
 		return $pages;
 	}
-	
+
 	public function filter_theme_act_display_home( $handled, &$theme )
 	{
 		$page= Options::get( 'staticfront__page' );
 		if ( $page && $page != 'none' ) {
 			$post= Post::get( array( 'slug' => $page ) );
 			$theme->act_display( array( 'posts' => $post ) );
-			return true;
+			return TRUE;
 		}
-		return false;
+		return FALSE;
 	}
-	
+
+	public function filter_theme_act_display_blog_home( $handled, &$theme )
+	{
+		$page= Options::get( 'staticfront__page' );
+		if ( $page && $page != 'none' ) {
+			$paramarray['fallback']= array(
+				'{$type}.multiple',
+				'multiple',
+			);
+ 
+			// Makes sure home displays only entries
+			$paramarray['user_filters']= array(
+				'content_type' => Post::type('entry'),
+			);
+ 
+			$theme->act_display( $paramarray );
+			return TRUE;
+		}
+
+		return FALSE;
+	}
+
 	public function filter_rewrite_rules( $rules )
 	{
 		if ( Options::get( 'staticfront__page' ) != 'none' ) {
+
 			$base= trim( Options::get( 'staticfront__blog_index' ) , '/' );
+
+			$move =  array(
+				'display_entries_by_date' => $base . '/',
+				'display_entry' => $base . '/',
+				'display_entries' => $base . '/',
+				'display_entries_by_tag' => $base . '/',
+				'display_search' => $base . '/',
+				'display_page' => $base . '/',
+				'display_404' => $base . '/',
+			);
+			if( Options::get( 'staticfront__keep_pages' ) ) {
+				$move['display_page'] = '';
+			}
+
+			foreach( $rules as $rule ) {
+				if( isset( $move[$rule->name] ) && strpos( $rule->build_str, $base ) === FALSE ) {
+					$rule->parse_regex= substr( $rule->parse_regex, 0, 2 ) . $move[$rule->name] . substr( $rule->parse_regex, 2 );
+					$rule->build_str= $move[$rule->name] . $rule->build_str;
+				}
+			}
+
 			$rules[] = new RewriteRule(array(
 				'name' => 'display_blog_home',
 				'parse_regex' => '%^' . $base . '(?:/page/(?P<page>\d+))?/?$%',
 				'build_str' => '' . $base . '(/page/{$page})',
 				'handler' => 'UserThemeHandler',
-				'action' => 'display_home',
+				'action' => 'display_blog_home',
 				'priority' => 1,
 				'rule_class' => RewriteRule::RULE_PLUGIN,
 				'is_active' => 1,
